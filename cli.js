739f4759b2027d6486e7a5bc86a61dca70ab7c7c
Alexandria-Anderson <aleand@ad.unc.edu>
2023-04-01 14:44:13 -0400

    First attempt to fix CRLF line endings

--- cli.js
+++ cli.js
@@ -18,12 +18,30 @@ if (process.argv[2] === '-h'){
     process.exit(0);
 }
 
-//if statements that loop through proces.argv, finding the flag
+if (process.argv[process.argv.indexOf('-n')] < 0 || process.argv[process.argv.indexOf('-e')] < 0 || process.argv[process.argv.indexOf('-s')] > 0 || process.argv[process.argv.indexOf('-w')] > 0){
+    process.exit(1);
+}
+if (process.argv[2] === '-j'){
+   /* should echo the JSON that your app ingested
+    from Open-Meteo onto STDOUT and exit 0. If you 
+    do not pass further parameters, this should 
+    return an error from the API. There should be 
+    no default input for the location variables.*/
+}
 
+if (process.argv[process.argv.indexOf('-d')] === 0){
+    console.log('today');
+} else if (process.argv[process.argv.indexOf('-d')] === 1 || process.argv[process.argv.indexOf('-d')]=== undefined){
+    console.log('tomorrow');
+} else if (process.argv[process.argv.indexOf('-d')] >= 2 || process.argv[process.argv.indexOf('-d')] <= 6){
+    console.log(`in ${process.argv[process.argv.indexOf('-d')]} days.`)
+}
 
 if (process.argv[2] === '-z'){
     const timezone = moment.tz.guess();
 }
+
+// need function to take in parcipitation and tell whether or not you need galoshes
 
 //some clues here:https://github.com/comp426-2023-spring/schedule/blob/main/06-manipulating-data.md
 //let URL = galo.sh.getdata();
