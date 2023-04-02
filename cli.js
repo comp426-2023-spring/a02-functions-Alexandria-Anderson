58677b320f05c5bb161c53c2b9e8df0b2c864122
Alexandria-Anderson <aleand@ad.unc.edu>
2023-04-01 20:22:40 -0400

    Added current weather

--- cli.js
+++ cli.js
@@ -10,9 +10,7 @@ const args = minimist(process.argv.slice(2));
 // Put args onto STDOUT
 console.log(args)
 // Get a URL
-
-
-if (process.argv[2] === '-h'){
+if (process.argv[2] === '-h'){ //works
     console.log(
     'Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE\n'+
     '-h            Show this help message and exit.\n'+
@@ -24,48 +22,6 @@ if (process.argv[2] === '-h'){
     );
     process.exit(0);
 }
-
-if (args.n < 0 || args.e < 0 || args.s > 0 || args.w > 0){
-    console.log("invalid")
-   // process.exit(1);
-}
-
- 
-if (args.d === 0){
-    console.log('today');
-} else if (args.d === 1 || args.d === undefined){
-    console.log('tomorrow');
-} else if (args.d >= 2 || args.d <= 6){
-    console.log(`in ${args.d} days.`)
-}
-
-/*if (argc === 4 && args.n !== undefiend && args.w !== undefiend){
-    //return tommorrows weather and get the timezone. 
-}
-
-if (argc === 5 && args.n !== undefiend && args.w !== undefiend && args.z !== undefiend){
-    //return tommorrows weather
-}
-
-if (argc === 6 && args.n !== undefiend && args.w !== undefiend && args.z !== undefiend && args.d === 0){
-    //return todays weather
-}
-*/
-if (args.z === undefined){
-    const timezone = moment.tz.guess();
-   console.log(timezone)
-} else {
-    const timezone = args.z
-    console.log(timezone)
-    
-}
-
-// need function to take in parcipitation and tell whether or not you need galoshes
-
-//some clues here:https://github.com/comp426-2023-spring/schedule/blob/main/06-manipulating-data.md
-
-
-//building URL
 let lat = null;
 if (args.n !== undefined){
      lat = parseFloat(args.n);
@@ -79,26 +35,60 @@ if (args.e !== undefined){
      long = parseFloat(args.w);
 }
 
-//rewriting timezone
+//timezone
+if (args.z === undefined){
+    const timezone = moment.tz.guess();
+   console.log(timezone)
+}
 let timezoneP = args.z.replace("/", "%2F");
 
-timezoneP = timezoneP.replace("Y", "_Y")
-
 console.log(timezoneP)
+let url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current_weather=true&daily=precipitation_hours,&timezone=${timezoneP}`
 
-let url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,uv_index_max,uv_index_clear_sky_max,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,precipitation_probability_max,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant,shortwave_radiation_sum,et0_fao_evapotranspiration&timezone=${timezoneP}`
+//checking to see which day is requested WIP
+if (args.d === 0){
+    //const response = await fetch(url)
+    console.log('today');
+} else if (args.d === 1 || args.d === undefined){
+   // url.replace('&current_weather=true&', '&')
+    console.log('tomorrow');
+} else if (args.d >= 2 || args.d <= 6){
+    console.log(`in ${args.d} days.`)
+}
 
-const response = await fetch(url)
+const response = await fetch(url) //works
 // Store the response JSON data in an object
 const data = await response.json();
 // Echo back the data to SDOUT
 let dataString = JSON.stringify(data);
-console.log(data);
-
-if (args.j !== undefined){
+console.log(data)
+if (args.j){//WIP
    /* should echo the JSON that your app ingested
     from Open-Meteo onto STDOUT and exit 0. If you 
     do not pass further parameters, this should 
     return an error from the API. There should be 
     no default input for the location variables.*/
+    if (args.length === 2){
+        throw console.error("No arguments");
+    } else {
+        console.log(data); 
+        process.exit(0)}
+   
+
 }
+
+
+
+if (args.n < 0 || args.e < 0 || args.s > 0 || args.w > 0){
+    console.log("invalid")
+   // process.exit(1);
+}
+
+ 
+
+
+// need function to take in parcipitation and tell whether or not you need galoshes
+
+//some clues here:https://github.com/comp426-2023-spring/schedule/blob/main/06-manipulating-data.md
+
+
